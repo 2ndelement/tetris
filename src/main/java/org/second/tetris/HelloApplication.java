f159f2746eff0154ccf279417a5a40db4a7ed691
npusamuelsun <1730462171@qq.com>
2021-12-15 20:55:47 +0800

    首页粗略的敲了出来

--- src/main/java/org/second/tetris/HelloApplication.java
+++ src/main/java/org/second/tetris/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello,World!");
+        Scene scene = new Scene(fxmlLoader.load(), 350, 500);
+        stage.setTitle("俄罗斯方块");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/second/tetris/HelloController.java
+++ src/main/java/org/second/tetris/HelloController.java
@@ -5,10 +5,25 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label homePage;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        homePage.setText("欢迎体验俄罗斯方块！");
     }
+
+    /*单人模式按钮 还需要改单机后效果*/
+    @FXML
+    protected void onSinglePlayerModeClick() {
+        homePage.setText("进入单机模式");}
+
+    /*得分记录按钮 */
+    @FXML
+    protected void onScoreRecordClick() {
+        homePage.setText("显示得分");}
+
+    /*游戏介绍*/
+    @FXML
+    protected void onGameIntroductionClick() {
+        homePage.setText("这个是游戏介绍");}
 }
