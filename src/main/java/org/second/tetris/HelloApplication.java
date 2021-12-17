bd3e68e228ed2bff8764637bc404b8618e9929c3
onecupof <96122897+onecupof@users.noreply.github.com>
2021-12-17 18:59:35 +0800

    Update HelloApplication.java

--- src/main/java/org/second/tetris/HelloApplication.java
+++ src/main/java/org/second/tetris/HelloApplication.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.second.tetris.utils.UserUtils;
 
-import java.io.IOException;
+import java.io.*;
 
 public class HelloApplication extends Application {
+    private UserUtils user;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,6 +17,16 @@ public class HelloApplication extends Application {
         stage.setTitle("俄罗斯方块");
         stage.setScene(scene);
         stage.show();
+        /**
+            创建路径，如果没有该文件，则新生成一个。
+         */
+        File newfile = new File("C:\\user.txt");
+        if(!newfile.exists()){
+            newfile.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\user.txt",true));
+            user = new UserUtils();
+            writer.write(user.generateName());
+        }
     }
 
     public static void main(String[] args) {
