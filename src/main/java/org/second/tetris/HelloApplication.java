aad2a1242639f14af2614a05380ac74cdd65fad3
onecupof <96122897+onecupof@users.noreply.github.com>
2021-12-17 18:59:35 +0800

    修正 HelloApplication.java

--- src/main/java/org/second/tetris/HelloApplication.java
+++ src/main/java/org/second/tetris/HelloApplication.java
@@ -4,9 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.second.tetris.entity.User;
+import org.second.tetris.utils.UserUtils;
 
-import java.io.IOException;
+import java.io.*;
 
+/**
+ * 主界面
+ *
+ * @author 柴福林
+ * @version 1.0
+ */
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -15,6 +23,17 @@ public class HelloApplication extends Application {
         stage.setTitle("俄罗斯方块");
         stage.setScene(scene);
         stage.show();
+        //如果没有该用户即创建一个文件存储信息。
+        try{
+            File newfile = new File("C:\\user.txt");
+            if(!newfile.exists()){
+                newfile.createNewFile();
+                BufferedWriter writer = new BufferedWriter(new FileWriter("user.txt",true));
+                writer.write(UserUtils.generateName());
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
