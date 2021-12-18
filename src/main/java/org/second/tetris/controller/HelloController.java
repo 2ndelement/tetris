ed6b13808872bb7d0a30ea10a0d7465f70eb3a43
onecupof <96122897+onecupof@users.noreply.github.com>
2021-12-18 20:22:11 +0800

    //界面跳转

--- src/main/java/org/second/tetris/controller/HelloController.java
+++ src/main/java/org/second/tetris/controller/HelloController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.second.tetris.HelloApplication;
+import org.second.tetris.Tetris;
 import org.second.tetris.test;
 
 import java.io.IOException;
@@ -30,10 +31,10 @@ public class HelloController {
      * 单人模式按钮 还需要改单机后效果
      */
     @FXML
-    protected void onSinglePlayerModeClick() throws IOException {
+    protected void onSinglePlayerModeClick() throws Exception {
         homePage.setText("进入单机模式");
         HelloApplication.close();//调出单机模式界面关闭主界面。
-        test test = new test();
+        Tetris test = new Tetris();
         Stage stage = new Stage();
         test.start(stage);
 
