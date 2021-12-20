108517677b1efbd987ed20addba0687f9b204bd7
HeliosDragon <1781631428@qq.com>
2021-12-20 22:03:05 +0800

    修改了初始页面的背景图

--- src/main/java/org/second/tetris/Tetris.java
+++ src/main/java/org/second/tetris/Tetris.java
@@ -102,6 +102,7 @@ public class Tetris extends Application {
         drawBackgroud();
         stage.setScene(scene);
         stage.setTitle("TETRIS");
+        stage.setResizable(false);//禁止用户修改窗口大小
         stage.show();
         for (int[] line : MESH) {
             Arrays.fill(line, 0);
